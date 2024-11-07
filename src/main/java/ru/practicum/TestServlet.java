a7fb251f2587745967b7e9921e98ed7cddd89d29
vl4ks <veli4koksenia@yandex.ru>
2024-11-07 14:31:04 +0500

    Сборка проекта на чистом Spring

---
+++ src/main/java/ru/practicum/TestServlet.java
@@ -0,0 +1,15 @@
+package ru.practicum;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp) throws IOException {
+        resp.getWriter().print("OK!");
+    }
+}
